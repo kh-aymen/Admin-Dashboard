2e1d6ff99c51cc64ac390bfc3afe29fa279a907f
khalfi aymen <khalfi.aymen2021@gmail.com>
2024-03-11 17:49:09 +0100

    update ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import Geography from './scenes/geography'
 import Overview from './scenes/overview'
 import Daily from './scenes/daily'
 import Monthly from './scenes/monthly'
+import BreakDown from "./scenes/breakdown"
 
 
 
@@ -40,6 +41,7 @@ const App = () => {
               <Route path='/overview' element={<Overview />} />
               <Route path='/daily' element={<Daily />} />
               <Route path='/monthly' element={<Monthly />} />
+              <Route path='/breakdown' element={<BreakDown />} />
             </Route>
           </Routes>
         </ThemeProvider>
